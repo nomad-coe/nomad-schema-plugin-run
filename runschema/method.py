85a67923ba477fe3ab753810d7a1449d6b779440
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2024-04-29 12:16:00 +0200

    Fix schema

--- runschema/method.py
+++ runschema/method.py
@@ -1472,7 +1472,7 @@ class Interaction(MSection):
     )
 
     atom_labels = Quantity(
-        type=str,
+        type=np.str_,
         shape=['n_interactions', 'n_atoms'],
         description="""
         Labels of the atoms described by the interaction. Can be a list of lists for interaction groupings.
@@ -1481,7 +1481,7 @@ class Interaction(MSection):
 
     atom_indices = Quantity(
         type=np.int32,
-        shape=[],
+        shape=['n_interactions', 'n_atoms'],
         description="""
         Indices of the atoms in the system described by the interaction. Can be a list of lists for interaction groupings.
         """,
